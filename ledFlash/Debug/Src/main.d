73edceacda0074a933de28ea705e2a0d4f82d6ba
muchomor32 <bartek@DESKTOP-4RNJJI8>
2026-02-24 14:55:19 +0100

    initial commit

---
+++ ledFlash/Debug/Src/main.d
@@ -0,0 +1,12 @@
+Src/main.o: ../Src/main.c ../Src/../Drivers/Device/stm32l476xx.h \
+ ../Inc/../Drivers/Core/core_cm4.h ../Inc/../Drivers/Core/core_cmInstr.h \
+ ../Inc/../Drivers/Core/cmsis_gcc.h ../Inc/../Drivers/Core/core_cmFunc.h \
+ ../Inc/../Drivers/Core/core_cmSimd.h \
+ ../Src/../Drivers/Device/system_stm32l4xx.h
+../Src/../Drivers/Device/stm32l476xx.h:
+../Inc/../Drivers/Core/core_cm4.h:
+../Inc/../Drivers/Core/core_cmInstr.h:
+../Inc/../Drivers/Core/cmsis_gcc.h:
+../Inc/../Drivers/Core/core_cmFunc.h:
+../Inc/../Drivers/Core/core_cmSimd.h:
+../Src/../Drivers/Device/system_stm32l4xx.h:
